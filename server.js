657e918a0d1e8d2bae6667d87fac487ae2c2ce29
Unknown <867573@gmail.com>
2018-03-16 21:09:39 +0530

    server added

---
+++ server.js
@@ -0,0 +1,14 @@
+
+var express = require("express");
+var app = express();
+var port = process.env.PORT || 3000;
+
+app.use(express.static("public"));
+
+app.get("/", function(req, res) {
+  res.sendfile(__dirname + "/index.html");
+});
+
+app.listen(port, function() {
+  console.log("server started at "+port);
+});
