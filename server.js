46546a266427f30075da6f0a24fc3056c51e975a
Unknown <867573@gmail.com>
2018-04-01 18:58:22 +0530

    service pages added

--- server.js
+++ server.js
@@ -9,6 +9,39 @@ app.get("/", function(req, res) {
   res.sendfile(__dirname + "/index.html");
 });
 
+app.get("/marriage-catering", function(req, res) {
+  res.sendfile(__dirname + "/marriagecatering.html");
+});
+
+app.get("/birthday-party", function(req, res) {
+  res.sendfile(__dirname + "/birthdayparty.html");
+});
+
+app.get("/engagement-party", function(req, res) {
+  res.sendfile(__dirname + "/engagementparty.html");
+});
+
+app.get("/house-warming", function(req, res) {
+  res.sendfile(__dirname + "/housewarming.html");
+});
+
+app.get("/valaikappu", function(req, res) {
+  res.sendfile(__dirname + "/valaikappu.html");
+});
+
+app.get("/corporate-catering", function(req, res) {
+  res.sendfile(__dirname + "/corporatecatering.html");
+});
+
+app.get("/traditional-food", function(req, res) {
+  res.sendfile(__dirname + "/traditionalfood.html");
+});
+
+app.get("/photography-videography", function(req, res) {
+  res.sendfile(__dirname + "/photography&videography.html");
+});
+
+
 app.listen(port, function() {
   console.log("server started at "+port);
 });
